269e28fb5240bcfd55f94b3468ad644f38cd2f6f
Neelima <neelimaakkiraju2001@gmail.com>
2024-03-11 18:40:04 +0530

    new changes

--- 3.3 Postman/index.js
+++ 3.3 Postman/index.js
@@ -18,15 +18,15 @@ app.post("/register", (req, res) => {
   res.sendStatus(201);
 });
 
-app.put("/user/angela", (req, res) => {
+app.put("/user/neelima", (req, res) => {
   res.sendStatus(200);
 });
 
-app.patch("/user/angela", (req, res) => {
+app.patch("/user/akkiraju", (req, res) => {
   res.sendStatus(200);
 });
 
-app.delete("/user/angela", (req, res) => {
+app.delete("/user/neelima", (req, res) => {
   //Deleting
   res.sendStatus(200);
 });
