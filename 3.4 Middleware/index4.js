b8f040126e852d8806969803fe1c907a27352e72
Neelima <neelimaakkiraju2001@gmail.com>
2024-03-13 15:11:49 +0530

    completed middleware function

--- 3.4 Middleware/index4.js
+++ 3.4 Middleware/index4.js
@@ -1,8 +1,34 @@
 import express from "express";
+import { dirname } from "path";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var band = "";
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.use(bodyParser.urlencoded({extended: true}));
+
+function bandName(req , res , next){
+  console.log(req.body);
+  band = req.body["street"] + req.body["pet"];
+  next();
+}
+app.use(bandName);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
+
+app.post("/submit" , (req, res)=>{
+  console.log(req.body)
+  res.send(`<h1>Your Band Name Is</h1><h2> ${band}✌️ </h2>`)
+})
+
+
+
+app.listen(port , () =>{
+  console.log(`Server Running on port ${port}`)
+})
